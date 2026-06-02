a511e5c9e41a45830a43c31395c6519bf9377ef1
GeovanySilva <122573552+GeovanySilva@users.noreply.github.com>
2026-06-02 17:28:34 -0500

    Add inventory functions to funciones.h

--- funciones.h
+++ funciones.h
@@ -1,7 +1,13 @@
-#ifndef FUNCIONES_H
-#define FUNCIONES_H
+#ifndef INVENTARIO_H
+#define INVENTARIO_H
 
-int suma(int a, int b);
-int resta(int a, int b);
+int menu();
+void Nombre_Productos(char nombres[][20], int n);
+void Precios(float precio[], int n);
+float precio_total(float precio[],int n);
+int mayor(float precio[], int n);
+int menor(float precio[], int n);
+float promedio(float precio[], int n);
+int encontrar_producto(char nombres[][20], int n);
 
 #endif
